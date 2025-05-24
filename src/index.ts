ebd7a71093b79cd26083ee9c54c8f45916026c09
Keisuke Yamanaka <2500024+1021ky@users.noreply.github.com>
2025-05-24 21:53:10 +0900

    cloud run functionsで実行できるようにする
    インポートパスを修正。TypeScriptではないためbaseurlが無視されることを考慮。
    エントリーポイントの関数はエクスポートされるようにする。

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,15 @@
 import * as functions from '@google-cloud/functions-framework';
-import logger from 'log/logger'
+import logger from './log/logger'
 
 /**
  * エントリーポイントとなるメイン関数
  *
  */
-function main() {
-    functions.http('myHttpFunction', (req, res) => {
-        logger.info('start')
-        // Your code here
+functions.http('main', (req, res) => {
+    logger.info('start')
+    // Your code here
 
-        // Send an HTTP response
-        res.send('OK');
-        logger.info('end')
-    });
-}
-
-main()
+    // Send an HTTP response
+    res.send('OK');
+    logger.info('end')
+});
