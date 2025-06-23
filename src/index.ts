2387c025ebdfa13f4f5e7f344b626c727ffb9d44
Keisuke Yamanaka <2500024+1021ky@users.noreply.github.com>
2025-06-23 15:22:05 +0900

    parseとhandleの処理が終わってからレスポンスを返す

--- src/index.ts
+++ src/index.ts
@@ -8,23 +8,21 @@ import { handleEvents } from './line-messaging-api/event/handleEvents';
  * エントリーポイントとなるメイン関数
  *
  */
-functions.http('main', (req, res) => {
+functions.http('main', async (req, res) => {
     logger.info('start')
     logger.debug(req.body)
 
-    parseRequest(req, config.lineChannelSecret)
-        .then((events: WebhookEvent[] | null) => {
-            if (!events) {
-                logger.error('Failed to parse events');
-                return;
-            }
+    try {
+        const events: WebhookEvent[] | null = await parseRequest(req, config.lineChannelSecret);
+        if (!events) {
+            logger.error('Failed to parse events');
+        } else {
             logger.debug('Parsed events:', events);
             handleEvents(events);
-        })
-        .catch(err => {
-            logger.error('Error parsing request:', err);
-            return;
-        });
+        }
+    } catch (err) {
+        logger.error('Error parsing request:', err);
+    }
 
     // パースに失敗したことをリクエストを送る側が知る必要はないため、HTTPレスポンスは常に同じように返す。
     res.send('OK');
