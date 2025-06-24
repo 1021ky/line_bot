0c07dc969b3a909484964f1d28152a08f7e0e5a1
Keisuke Yamanaka <2500024+1021ky@users.noreply.github.com>
2025-06-24 22:55:28 +0900

    メンションされたことをそのまま返すようにする

--- src/line-messaging-api/event/handleEvents.ts
+++ src/line-messaging-api/event/handleEvents.ts
@@ -1,7 +1,14 @@
-import { JoinEvent, LeaveEvent, MessageEvent, WebhookEvent, EventMessage } from '@line/bot-sdk';
+import { JoinEvent, LeaveEvent, MessageEvent, WebhookEvent, EventMessage, Group } from '@line/bot-sdk';
 import logger from '../../log/logger';
 import { TextEventMessageWithIsSelf } from '../../types/external/text-event-message-with-isself';
+import { messagingApi } from '@line/bot-sdk';
 
+/**
+ * LINE Messaging APIのWebhookイベントを処理する関数
+ *
+ * @param events - Webhookイベントの配列
+ */
+// TODO: 具体的な処理内容はドメイン層で定義して、引数で渡すようにする
 /**
  * LINE Messaging APIのWebhookイベントを処理する関数
  * @param events WebhookEventの配列
@@ -29,7 +36,33 @@ function handleEventMessage(event: MessageEvent) {
     if (message.type === 'text') {
         const textMessage = message as TextEventMessageWithIsSelf;
         if (textMessage.mention && textMessage.mention.mentionees && textMessage.mention.mentionees.some(m => m.isSelf)) {
-            logger.info('I was said: ' + textMessage.text);
+            const client = createLineClient();// TODO: DIで注入するようにする
+            const to = isGroupEventSource(event) ? (event.source as Group).groupId : '';
+            if (to) {
+                const replyMessage = `You mentioned me with: ${textMessage.text}`; // 返信メッセージを生成は今は簡単な例にしているが、ここがドメインロジックになる
+                sendReply(client, to, replyMessage);
+            }
         }
     }
+}
+
+
+
+function isGroupEventSource(event: WebhookEvent): event is MessageEvent {
+    return event.type === 'message' && event.source.type === 'group';
+}
+
+function createLineClient() {
+    const client = new messagingApi.MessagingApiClient({
+        channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN || '',
+    });
+    return client;
+}
+
+function sendReply(client: messagingApi.MessagingApiClient, to: string, message: string) {
+    client.pushMessage(({
+        to: to,
+        messages: [{ type: 'text', text: message }]
+    }));
+
 }
